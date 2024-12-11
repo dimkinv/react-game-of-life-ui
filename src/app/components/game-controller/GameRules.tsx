65c7e6a0cb51e78cb79f692f24f9c2ab6d6d1fcf
Danny Vernovsky <dv696w@intl.att.com>
2024-12-11 12:24:09 +0200

    removed solution

--- src/app/components/game-controller/GameRules.tsx
+++ src/app/components/game-controller/GameRules.tsx
@@ -15,50 +15,7 @@ export const useGameRules = () => {
 
 
 function calculateTick(data: GridData): GridData{
-	const newGrid: GridData = [];
+	
 
-	for(let i = 0; i < data.length; i++){
-		for (let j = 0; j < data[i].length; j++) {
-			const element = data[i][j];
-
-			let liveNeighbors = 0;
-			for (const vector of vectors){
-				const neighbor = data[i + vector[1]]?.[j + vector[0]]
-				if(neighbor){
-					liveNeighbors++;
-				}
-			}
-
-			const newState = calculateState(element, liveNeighbors);
-			newGrid[i] = newGrid[i] ?? [];
-			newGrid[i][j] = newState;
-		}
-	}
-
-	return newGrid;
+	return data;
 }
-
-function calculateState(currentState:boolean, liveNeighbors: number) {
-	if(liveNeighbors < 2){
-		return false;
-	} else if (currentState && (liveNeighbors === 2 || liveNeighbors === 3) ){
-		return true;
-	} else if(liveNeighbors > 3){
-		return false;
-	} else if (!currentState && liveNeighbors === 3){
-		return true;
-	}
-
-	return currentState;
-}
-
-const vectors: [number,number][] = [
-	[-1, -1],
-	[0, -1],
-	[1, -1],
-	[1, 0],
-	[1, 1],
-	[0, 1],
-	[-1, 1],
-	[-1, 0]
-]
